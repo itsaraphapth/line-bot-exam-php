09ff738e252d5f85735a38f1745dae51c1a729e7
itsaraphapth <itsaraphap@icloud.com>
2018-07-02 14:08:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'tDNXAfNlKLIqC4GMCie32m1bdYju/SLgne1aJsASab999F+LBLJ6lt0EgnUAmWlg28Gdg6ARoJQIdk7LU3fWImZntWSZyNbwPqWfZPY4H4FbKdqpkPvVzC8F6tj7ER6M6d4e7uVRYYilL3zYlUpHTwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
