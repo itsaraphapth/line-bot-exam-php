5b476918d18ec613ca7897fb7bcac68aa7264069
ninjaerp <itsaraphap.t@gmail.com>
2018-07-06 14:53:01 +0700

    test

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'U11fae07ce7afb4aac7875be082b2b3ee';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีเอกสารใหม่ รออนุมัติ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
