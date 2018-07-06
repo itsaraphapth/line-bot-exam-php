5d2de6784b46e7ad04bb0cad2f6420be4c8fdc7a
ninjaerp <itsaraphap.t@gmail.com>
2018-07-06 16:37:08 +0700

    fa

--- botpush.php
+++ botpush.php
@@ -1,36 +1,87 @@
-<?php
 
 
 
-require "vendor/autoload.php";
-
-$access_token = 'dh1wWTCSKQGOZg5QQdtQuXvSMbPP7YsgS5ZDVv5dfNNKCChgttDTvwqz/PxbdplN28Gdg6ARoJQIdk7LU3fWImZntWSZyNbwPqWfZPY4H4EuoDVcCNLST5izrdeHG308idXPWYrN11ZoTzijjKvy9wdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = '3c69a807c14b2357a8ee4aae1b33dc38';
-
-$pushID = array( 
-    'U11fae07ce7afb4aac7875be082b2b3ee'
-
-    );
-    foreach($pushID as $val){
 
 
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-        
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("เอกสาร PR ใหม่ รออนุมัติ ".." ");
-        $response = $bot->multicast($pushID, $textMessageBuilder);
-        
-        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-        
 
+
+    <?php
+    require_once __DIR__ . '/vendor/autoload.php';
+  
+    // debug
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+  
+    // Channel secret - (from https://developers.line.me/console/)
+    $token = 'dh1wWTCSKQGOZg5QQdtQuXvSMbPP7YsgS5ZDVv5dfNNKCChgttDTvwqz/PxbdplN28Gdg6ARoJQIdk7LU3fWImZntWSZyNbwPqWfZPY4H4EuoDVcCNLST5izrdeHG308idXPWYrN11ZoTzijjKvy9wdB04t89/1O/w1cDnyilFU=';
+    // $token = $_POST['token'];
+    
+    // Channel access token - (from https://developers.line.me/console/)
+    $secret = '3c69a807c14b2357a8ee4aae1b33dc38';
+    // $secret = $_POST['secret'];
+  
+  
+    // connect key setup
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
+  
+    if(isset($_POST['to']) && trim($_POST['to']) != '' && isset($_POST['text']) && trim($_POST['text']) != ''){
+  
+      // check for send message only
+      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($_POST['text']);
+      $response = $bot->pushMessage($_POST['to'], $textMessageBuilder);
+  
+      // check status sending line api
+      if($response->isSucceeded()){
+        echo "true";
+      }else{
+        echo "false";
+      }
+  
+    }else{
+  
+      // Get POST body content
+      $content = file_get_contents('php://input');
+      // Parse JSON
+      $events = json_decode($content, true);
+  
+      // Validate parsed JSON data
+      if (!is_null($events['events'])) {
+        foreach ($events['events'] as $event) {
+          if($event['type'] == "message" && isset($event['message']['text'])){
+  
+            $type = $event['source']['type']; // user , room , group
+            $to = $event['source'][$type.'Id']; // userId , roomId , groupId
+            $message = trim($event['message']['text']);
+  
+            switch ($message) {
+              case '/help':
+                $text = "ฉันคือ ID Finder Bot ยินดีที่ได้รู้จัก";
+                $text .= "\nฉันมีหน้าที่ช่วยคุณค้าหา UserID RoomID หรือ GroupID ให้กับคุณ";
+                $text .= "\nลองพิมพ์ /id ดูซิ";
+                break;
+              case '/id':
+                $text = "ข้อมูล ID ของคุณ";
+                if(isset($event['source']['userId'])){ $text .= "\nUser ID : ".$event['source']['userId']; }
+                if(isset($event['source']['roomId'])){ $text .= "\nRoom ID : ".$event['source']['roomId']; }
+                if(isset($event['source']['groupId'])){ $text .= "\nGroup ID : ".$event['source']['groupId']; }
+                break;
+              default:
+                $text = NULL;
+                break;
+            }
+  
+            // message setup & send
+            if($text != NULL){
+              $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
+              $response = $bot->pushMessage($to, $textMessageBuilder);
+            }
+  
+          }
+        }
+      }
     }
-
-
-
-
-
-
-
-
-
+  
+    // debug
+    // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+  ?>
