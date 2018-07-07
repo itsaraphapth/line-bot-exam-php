2713750ee7329d40a10bb76444fddcdc2678e061
ninjaerp <itsaraphap.t@gmail.com>
2018-07-07 12:25:43 +0700

    test

--- botpush.php
+++ botpush.php
@@ -19,11 +19,11 @@
     // Channel access token - (from https://developers.line.me/console/)
     $secret = '3c69a807c14b2357a8ee4aae1b33dc38';
     // $secret = $_POST['secret'];
-//    $pushID = array(
-//        "U11fae07ce7afb4aac7875be082b2b3ee",
-//        "U0e6b5794496cbcee1bb4850c8f888c8c",
-//        "U8f70ff048d6c81f89cc0f280be0acef2"
-//    );
+   $pushID = array(
+       "U11fae07ce7afb4aac7875be082b2b3ee",
+       "U0e6b5794496cbcee1bb4850c8f888c8c",
+       "U8f70ff048d6c81f89cc0f280be0acef2"
+   );
   
     // connect key setup
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
@@ -32,8 +32,8 @@
     if(isset($_POST['to']) && trim($_POST['to']) != '' && isset($_POST['text']) && trim($_POST['text']) != ''){
   
       // check for send message only
-      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($_POST['text']);
-      $response = $bot->multicast($_POST['to'], $textMessageBuilder);
+      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ลองส่งหลายคน");
+      $response = $bot->multicast($pushID, $textMessageBuilder);
   
       // check status sending line api
       if($response->isSucceeded()){
